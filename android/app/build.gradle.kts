575c5d80be62611d24257ce356685a5467de37c0
Yousif Khalid <yousifkhalid544@gmail.com>
2026-05-07 10:19:47 -0700

    feat: implement dev and prod build flavors with environment-specific entry points

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -33,6 +33,20 @@ android {
         versionName = flutter.versionName
     }
 
+    flavorDimensions.add("default")
+    productFlavors {
+        create("dev") {
+            dimension = "default"
+            applicationIdSuffix = ".dev"
+            resValue ("string", "app_name", "SuperManga DEV")
+        }
+        create("prod") {
+            dimension = "default"
+            resValue("string", "app_name", "SuperManga PROD")
+        }
+    }
+
+
     buildTypes {
         release {
             // TODO: Add your own signing config for the release build.
